4b5fd69ac7e0d4cf76369b4b621ddcd1963b6d0b
John D. Aynedjian <johndavid5@yahoo.com>
2017-12-14 20:21:21 -0500

    copydist.bat...

---
+++ scripts/copydist.bat
@@ -0,0 +1,4 @@
+mv ./static/bootstrap ./static/bootstrap.txt
+cp -r ./node_modules/bootstrap/dist ./static/bootstrap
+mv ./static/react-select ./static/react-select.txt
+cp -r ./node_modules/react-select/dist ./static/react-select
